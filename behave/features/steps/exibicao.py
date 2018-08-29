2bdd644670cf2a30d0948e46af22520825d54cee
Josias Wando <josiasn.queiroz@gmail.com>
2018-08-29 15:09:44 -0300

    Pequenos ajustes no requiriments.txt

--- behave/features/steps/exibicao.py
+++ behave/features/steps/exibicao.py
@@ -1,9 +1,15 @@
 from behave import *
+import time
 
 @given("que estou no caminho file:///home/josias/Documentos/revistas/web-app/index.html")
 def openIndex(context):
-    context.browser.get('file:///home/josias/Documentos/revistas/web-app/index.html')
+    try:
+        revista = context.browser.get('file:///home/josias/Documentos/revistas/web-app/index.html')
+        assert True
+    except Exception as e:
+        raise EnvironmentError(e)
+     
 
-@then("devo visualizar a lista \"lista_de _revistas\"")
-def step_impl(context):
+@then("devo visualizar o texto \"revistas em estoque\"")
+def seeList(context):
     assert context.failed is False
